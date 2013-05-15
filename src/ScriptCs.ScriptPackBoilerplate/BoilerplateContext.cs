a04a0377f4cc7f6509b0f94335127e9bbd6e0d37
Scott Smith <scottksmith95@gmail.com>
2013-05-15 14:01:36 -0700

    Initial commit

---
+++ src/ScriptCs.ScriptPackBoilerplate/BoilerplateContext.cs
@@ -0,0 +1,11 @@
+﻿using ScriptCs.Contracts;
+
+namespace ScriptCs.ScriptPackBoilerplate
+{
+    public class BoilerplateContext : IScriptPackContext
+    {
+        //Here is where you will implement your script pack's
+        //functionality. This is the object returned by the script's
+        //call to Require<ScriptPackContext>()
+    }
+}
